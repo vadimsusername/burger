161bcb4b6ec654bf026c4e76a4f47fad08fd3855
vadimsusername <vadimburyakovsky@yahoo.com>
2020-03-13 02:42:24 -0400

    added folders and files to create the directory structure

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var burger_model = require("../models/burger.js");
+var router = express.Router();
+
+
+
+
+
+
+
+
+
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO burgers (burger_name) VALUES ("impossible burger");
+INSERT INTO burgers (burger_name) VALUES ("big mac");
+INSERT INTO burgers (burger_name) VALUES ("whopper");
